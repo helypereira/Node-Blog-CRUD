e0e7dfdf90d5ccceeaa38030e4a6377257d43222
helypereira <almeidahely@gmail.com>
2023-12-27 22:08:06 -0300

    ## Commit Message
    **Changes:**
    - Migrate from ES6 module syntax to CommonJS for compatibility.
    - Integrate MongoDB database for persistent storage of blog entries.
    - Implement file upload functionality using Multer.
    - Refactor the rendering of image paths for better display in the UI.
    
    **Context:**
    This commit marks a significant update to the project structure and functionality. The migration to CommonJS syntax ensures compatibility, and MongoDB has been integrated for robust and persistent data storage. Multer is now used for handling file uploads, and image paths are refactored to display correctly in the UI.

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# Node-Blog-CRUD
-Express-Blog-App
+# Node-Blog-CRUD (MongoBlog)
 
+MongoBlog is a simple blog application developed with Node.js, Express, MongoDB, and EJS. This application allows users to easily create, read, update, and delete blog entries. It leverages MongoDB for data storage and Express as the web server framework.
 
-This code represents the implementation of a web server using Node.js and Express for a simple blog system. It allows basic Create, Read, Update, and Delete (CRUD) operations for blog entries, including support for images. Additionally, it utilizes the EJS template engine to dynamically generate the blog views.
+## Key Features
 
-The web application includes routes to view all entries, add new entries, edit and delete existing entries, as well as view specific details of an entry. The Multer middleware is used to handle the uploading of images associated with each entry. The code is modularly structured and follows Express conventions for route and view handling.
+- **Full CRUD Operations:** Perform basic Create, Read, Update, and Delete (CRUD) operations on blog entries.
+- **MongoDB Storage:** Uses a MongoDB database to store and retrieve blog entries.
+- **Image Support:** Enables users to attach images to their blog entries for a more visual experience.
+- **Dynamic Date:** Displays the creation or editing date of an entry dynamically using Moment.js.
+- **EJS Views:** Utilizes the EJS template engine to dynamically generate HTML views on the server.
+- **Multer for File Uploads:** Handles file uploads, especially images, using the Multer library.
+- **Simple Style and Design:** Features a clean and minimalist design for an intuitive user experience.
 
-This project provides a practical introduction to essential web development concepts with Node.js and Express and can serve as a starting point for more complex projects or for learning about web application development with JavaScript.
+## Usage Instructions
+
+1. Clone this repository to your local machine.
+2. Install dependencies with `npm install`.
+3. Configure your MongoDB connection in the `index.js` file.
+4. Run the application with `npm start`.
+
+Explore, create, and share your thoughts with MongoBlog!
+
+## Contributions
+
+Contributions are welcome! If you find bugs, have suggestions, or improvements, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License
--- index.js
+++ index.js
@@ -1,149 +1,180 @@
-import express from "express";
-import bodyParser from "body-parser";
-import multer from "multer";
-import data from "./data.js"
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+import path from 'path';
+import moment from 'moment';
+//import connection from './config.js';
+
+
+const connection = process.env.BLOG_DB_CONNECTION;
+
+if (!connection) {
+    console.error("Error: La variable de entorno BLOG_DB_CONNECTION no está configurada.");
+    process.exit(1); // Ends the process with a non-successful exit code (1).
+}  
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Configuración de multer para almacenar imágenes en la memoria
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-// Arreglo que almacenará las entradas del blog
-const posts = [];
-// Variable para mantener un seguimiento del último ID asignado a una entrada
-let currentId = 0;
-
-// Carga de datos iniciales desde el archivo data.js
-data.forEach(item => {
-    const info = {
-        id: item.id,
-        title: item.title,
-        content: item.content,
-    };
-    // Se agrega la entrada al arreglo de posts
-    posts.push(info);
-    currentId++
-});
-
-app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({ extended: true }));
+// Configuración del motor de plantillas EJS
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", { posts: posts });
+// Configuración para manejar la carga de archivos con multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
 });
 
-// Ruta POST para enviar una nueva entrada al blog
-app.post("/submit", upload.single('post-image'), (req, res) => {
-    const title = req.body["title"];
-    const content = req.body["content"];
-    const image = req.file;
-    
-    // Creación de una nueva entrada
-    const blogEntry = {
-        id: currentId++,
-        title: title,
-        content: content,
-        image: image, 
-    } 
-    // Validación de título y contenido antes de agregar la entrada
-    if(blogEntry.title && blogEntry.content){
-        posts.push(blogEntry);
-    }else {
-        // Si la validación falla, se vuelve a renderizar la vista index.ejs
-        res.render("index.ejs", { posts: posts });
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        const filetypes = /jpeg|jpg|png/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+        cb('Error: Solo se permiten imágenes (jpeg/jpg/png).');
     }
-    // Se renderiza la vista index.ejs con las entradas actualizadas
-    res.render("index.ejs", { posts: posts });
 });
 
-app.get("/post", (req, res) => {
-    res.render("post.ejs");
-});
+// Conexión a la base de datos
+mongoose.connect(connection)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
 
-// Ruta para editar una entrada específica
-app.get("/edit/:id", (req, res) => {
-    const postId = parseInt(req.params.id);
-    // Buscar la entrada en el arreglo de posts
-    const postToEdit = posts.find(post => post.id === postId);
+// Definición del modelo
+const Post = mongoose.model('Post', {
+    title: String,
+    content: String,
+    imagePath: String,
+    date: { type: Date, default: Date.now },
+}, 'info');
 
-    // Validación: si la entrada no se encuentra, se devuelve un error 404
-    if (!postToEdit) {
-        res.status(404).send("Post not found");
-        return;
+// Middleware para procesar datos de formularios
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Ruta principal que muestra las entradas de blog
+app.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: 'desc' });
+        const postsWithImagePath = posts.map(post => ({
+            ...post.toObject(),
+            imagePath: post.imagePath ? `/uploads/${path.basename(post.imagePath)}` : null,
+        }));
+        res.render('index', { posts: postsWithImagePath, moment });
+    } catch (error) {
+        console.error('Error fetching posts:', error.message);
+        res.status(500).send('Internal Server Error');
     }
-
-    // Renderizar la vista edit.ejs con la entrada específica
-    res.render("edit.ejs", { post: postToEdit });
 });
 
-// Ruta para ver detalles de una entrada específica
-app.get("/read/:id", (req, res) => {
-    const postId = parseInt(req.params.id);
-    // Buscar la entrada en el arreglo de posts
-    const postToRead = posts.find(post => post.id === postId);
-
-    // Validación: si la entrada no se encuentra, se devuelve un error 404
-    if (!postToRead) {
-        res.status(404).send("Post not found");
-        return;
+// Ruta para mostrar la vista de lectura de una entrada
+app.get('/read/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).send('Post not found');
+        }
+        res.render('read', { post, moment });
+    } catch (error) {
+        console.error('Error fetching post for reading:', error.message);
+        res.status(500).send('Internal Server Error');
     }
-    // Renderizar la vista read.ejs con la entrada específica
-    res.render("read.ejs", { post: postToRead });
 });
 
-// Ruta POST para editar una entrada específica
-app.post("/edit/:id", upload.single('post-image'), (req, res) => {
-    const postId = parseInt(req.params.id);
-    // Encontrar el índice de la entrada a editar en el arreglo de posts
-    const postToEditIndex = posts.findIndex(post => post.id === postId);
+// Ruta para mostrar el formulario de creación de entrada
+app.get('/post', (req, res) => {
+    res.render('post');
+});
 
-     // Validación: si la entrada no se encuentra, se devuelve un error 404
-    if (postToEditIndex === -1) {
-        res.status(404).send("Post not found");
-        return;
+// Ruta para procesar el formulario de creación de entrada
+app.post('/post', upload.single('image'), async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const imagePath = req.file ? req.file.path : null;
+
+        const newPost = new Post({ title, content, imagePath });
+        await newPost.save();
+
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error creating post:', error.message);
+        res.status(500).send('Internal Server Error');
     }
-
-    // Creación de una entrada editada
-    const editedPost = {
-        id: postId,
-        title: req.body.title,
-        content: req.body.content,
-        image: req.file || posts[postToEditIndex].image,
-    };
-
-    // Reemplazar la entrada antigua con la entrada editada en el arreglo de posts
-    posts[postToEditIndex] = editedPost;
-    res.redirect("/");
 });
 
-
-
-
-// Ruta para eliminar una entrada específica
-app.get("/delete/:id", (req, res) => {
-    const postId = parseInt(req.params.id);
-    // Encontrar el índice de la entrada a eliminar en el arreglo de posts
-    const postToDeleteIndex = posts.findIndex(post => post.id === postId);
-
-    // Validación: si la entrada no se encuentra, se devuelve un error 404
-    if (postToDeleteIndex === -1) {
-        res.status(404).send("Post not found");
-        return;
+// Ruta para mostrar un formulario de edición de entrada
+app.get('/edit/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).send('Post not found');
+        }
+        res.render('edit', { post, moment });
+    } catch (error) {
+        console.error('Error fetching post for editing:', error.message);
+        res.status(500).send('Internal Server Error');
     }
-
-    // Eliminar la entrada del arreglo de posts
-    posts.splice(postToDeleteIndex, 1);
-
-    res.redirect("/");
 });
 
-// delete
+// Ruta para procesar el formulario de edición de entrada
+app.post('/edit/:id', upload.single('image'), async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id,
+            { title, content, imagePath: req.file ? req.file.path : null, date: Date.now() },
+            { new: true }
+        );
+        if (!updatedPost) {
+            return res.status(404).send('Post not found');
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error updating post:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
+// Ruta para mostrar un formulario de eliminación de entrada
+app.get('/delete/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).send('Post not found');
+        }
+        res.render('delete', { post, moment });
+    } catch (error) {
+        console.error('Error fetching post for deletion:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+// Ruta para procesar el formulario de eliminación de entrada
+app.post('/delete/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        await Post.findByIdAndDelete(postId);
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error deleting post:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
